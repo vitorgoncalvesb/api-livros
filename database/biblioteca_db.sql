2a49258b14fc703ca1f11a0ea8c6d8b4e6c395b1
vitorgoncalvesb <vitor.goncalvesb1@gmail.com>
2026-09-10 07:47:20 -0300

    docs: atualiza banco apos rotas de cadastro e consulta

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Set-2026 às 13:14
+-- Tempo de geração: 10-Set-2026 às 12:46
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -22,6 +22,48 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `biblioteca_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `biblioteca_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `autor` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ano_publicacao` int(11) NOT NULL,
+  `disponivel` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) VALUES
+(1, 'O Hobbit', 'J. R. R. Tolkien', 1937, 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ix_livros_id` (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
