6ce01c57c7ace9370e60d959d0c2c32d5eeb28ff
vitorgoncalvesb <vitor.goncalvesb1@gmail.com>
2026-09-10 07:41:56 -0300

    feat: cria consulta de livro por id

--- app/main.py
+++ app/main.py
@@ -39,3 +39,15 @@ def listar_livros(sessao_banco: Session = Depends(obter_sessao_banco)):
     livros = resultado.scalars().all()
 
     return livros
+
+
+@app.get("/livros/{id_livro}", response_model=LivroResposta, tags=["Livros"])
+def obter_livro(id_livro: int, sessao_banco: Session = Depends(obter_sessao_banco)):
+    consulta = select(Livro).where(Livro.id == id_livro)
+    resultado = sessao_banco.execute(consulta)
+    livro = resultado.scalar_one_or_none()
+
+    if livro is None:
+        raise HTTPException(status_code=404, detail="Livro não encontrado")
+
+    return livro
